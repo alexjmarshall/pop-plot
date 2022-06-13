f578f243f35f7144f9be07d18e13edd07596be57
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-06-13 17:03:29 -0600

    transform csv plot data and pass to plot component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,31 @@ export default function App() {
 
     const fetchData = async () => {
       const csvURL = 'https://gist.githubusercontent.com/low-sky/bec36274c4bf28619e503e2ae6a59d3a/raw/5dbc063e0a954a88df283a046f996c586ad20fb6/EgyptCities.csv';
+      const makePlotData = datapoints => {
+        // datapoint example:
+        // {
+        //  Name: "Cairo"
+        //  lat: "30.0444"
+        //  lng: "31.2358"
+        //  population: "19787000"
+        // }
 
+        // TODO make AREA proportional to pop. not radius of the circle
+
+        const data = datapoints.map(p => ({
+          x: p.lat,
+          y: p.lng,
+          r: p.population,
+        }));
+
+        setPlotData({
+          datasets: [{
+            label: 'Egyptian Cities',
+            data,
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          }]
+        });
+      };
 
       d3.csv(csvURL).then(makePlotData);
     };
